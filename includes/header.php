dd84c2e9e39a3664d28b363578d76adf34540a6a
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-10-24 08:24:40 +0000

    Organize/refactor code. Change main file extension to php

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+
+    <!-- MetaTags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap CDN | CSS stylesheets -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/homepage.css">
+
+    <!-- Sourca Sans Pro Font Family -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+
+    <!-- Page title -->
+    <title>Paradise Guide | Bem-vindo</title>
+</head>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!-- Header -->
+<?php include("includes/header.php"); ?>
+
+<body>
+
+    <!-- Homepage body -->
+    <div class="homepage-bg">
+
+        <!-- Navbar -->
+        <?php include("includes/navbar.php"); ?>
+        
+        <!-- Login and register forms -->
+        <?php include("includes/forms.php"); ?>
+
+    </div>        
+
+    <!-- Footer -->
+    <?php include("includes/footer.php"); ?>
+
+</body>
+</html>
