d638eee641085e0bfa85945ea8e9fc0f6a3a7e42
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-11-02 10:13:37 -0100

    Add logo to main index and client area. Adjust respective navbars (search option disabled for now)

--- includes/area_cliente_navbar.php
+++ includes/area_cliente_navbar.php
@@ -1,6 +1,6 @@
 <!-- Navbar --> 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="area_cliente.php">LOGO AQUI</a>
+    <a href="area_cliente.php"><img class="logo_area_cliente" src="img/logo.png"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -17,10 +17,11 @@
                 <a class="nav-link" href="#">Todas as atividades</a>
             </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
+        <!-- <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2 pesquisa_area_cliente" type="text" name="pesquisa_area_cliente" placeholder="Pesquise qualquer atividade...">
             <button class="btn btn-secondary my-2 my-sm-0" type="submit">Pesquisar!</button>
-        </form>
+        </form> -->
+        
         <a href="includes/cliente_logout.php"><button class="btn btn-primary logout_button">Encerrar sessão</button></a>
     </div>
 </nav>
