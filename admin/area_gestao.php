6001e79ff288becda1507100908399ccdd5d6795
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-11-06 22:12:59 -0100

    Add assets to admin. Start to create the structure for the management area

---
+++ admin/area_gestao.php
@@ -0,0 +1,11 @@
+<body>
+    
+    <!-- Incluir o header da área de gestão --> 
+    <?php include("includes/includes_area_gestao/area_gestao_header.php"); ?>
+
+    MENU LATERAL AQUI
+       
+    <!-- Incluir o footer da área de gestão --> 
+    <?php include("includes/includes_area_gestao/area_gestao_footer.php"); ?>
+
+</body>
--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,46 @@
-<!-- Header do índex da área admin -->
-<?php include_once("admin_includes/index_header.php"); ?>
+<!-- Incluir a classe Database -->
+<?php include_once("../data/database.php"); ?>
+
+<!-- Definir as messagens de erro de login como vazia inicialmente -->
+<?php $error_message_login = ""; ?>
+
+<!-- Session start --> 
+<?php session_start(); ?>
+
+<?php 
+
+    /* Um cliente não poderá ter acesso à área admnistrativa (é redirecionado para a 
+    área de cliente) */
+    if (isset($_SESSION["client"])) {header("Location:area_cliente.php");}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+
+    <!-- MetaTags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Custom CSS stylesheets -->
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+
+    <!-- Ionic icons -->
+    <script src="https://unpkg.com/ionicons@4.4.6/dist/ionicons.js"></script>
+
+    <!-- Source Sans Pro Font Family -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+
+    <!-- Page title -->
+    <title>Paradise Guide | Área de Gestão</title>
+    
+</head>
 
 <body>
 
