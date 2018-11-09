176bd25ba1748960e66ef2027af1c0dd4bc48cfd
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-11-09 10:53:51 -0100

    Add admin logout

---
+++ admin/includes/includes_area_gestao/admin_logout.php
@@ -0,0 +1,15 @@
+<!-- Funcionalidade de logout --> 
+<?php 
+
+    # Verificar se o utilizador não chegou à página pelo link
+    session_start();
+        if(!isset($_SESSION["admin"])) {
+    }
+
+    # Destruir a variável de sessão
+    session_destroy();
+    
+    # Redirecionar o cliente para o índex
+    header('location:../../index.php');
+
+?>
